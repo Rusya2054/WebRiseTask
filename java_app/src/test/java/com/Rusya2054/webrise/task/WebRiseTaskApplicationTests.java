ad76ed8b8c7c94ebb9d1d489e5fa29b9df5bc77c
aubakirov <Aubakirov@togi.ru>
2025-05-13 19:06:32 +0500

    - added init.sql - updated application.yaml and .gitignore

--- java_app/src/test/java/com/Rusya2054/webrise/task/WebRiseTaskApplicationTests.java
+++ java_app/src/test/java/com/Rusya2054/webrise/task/WebRiseTaskApplicationTests.java
@@ -1,13 +1,13 @@
-package com.Rusya2054.webrise.task;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WebRiseTaskApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.Rusya2054.webrise.task;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WebRiseTaskApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
